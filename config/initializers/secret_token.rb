7a707bff5aa4d73aaa0306fe8f8b9ce832812189
Ordie Dordie <politicalarnold@gmail.com>
2012-04-09 19:11:22 -0700

    now committing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playingwithrails::Application.config.secret_token = '80eb073c428f08f83e9f8587a1d8797bbc6e1b4903888de725178c4c7082a47ab831bae517391e54a6e0eb9b0815edb405de02df2091e3d90f84649880f28a7c'
